578629e3ae1f462e134251dad608f29d933f8156
Jakedez <jakedez7@gmail.com>
2022-11-08 09:42:29 -0700

    Added Dictionary Class

---
+++ Dictionary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSE210_03{
+    public class Dictionary{
+        private List<string> words = new List<string>() {
+            "fluffy",
+            "postal",
+            "forge",
+            "salami",
+            "stomp",
+            "alter",
+            "something",
+            "rythym"
+        };
+        private Random random = new Random();
+        private string currentWord = "";
+        public string getWord(){
+            return currentWord;
+        }
+        public void getRandomWord(){
+            currentWord = words[random.Next(words.Count())];
+        }
+        public Dictionary(){
+            getRandomWord();           
+        }
+    }
+}
