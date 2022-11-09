689652f7274be98ef95502879656602dea061adf
JoeOlsenBYUI <ols12023@byui.edu>
2022-11-09 10:59:53 -0700

    add Parachute class and methods

--- Game/Parachute.cs
+++ Game/Parachute.cs
@@ -1,11 +1,55 @@
 using System;
+using System.Collections.Generic;
 
-
-namespace Game
+namespace CSE210_03
 {
     /// Parachute
     public class Parachute
     {
+        List<string> jumper = new List<string>();
+        
+        int listRange;
+
+
+        public Parachute()
+        {
+            jumper.Add("  / \\  ");
+            jumper.Add("  /|\\  ");
+            jumper.Add("   0   ");         
+            jumper.Add("  \\ /  ");
+            jumper.Add(" \\   / ");
+            jumper.Add(" /___\\ ");
+            jumper.Add("  ___  ");
+
+            listRange = jumper.Count;
+        }
+
+        public void wrongGuess()
+        {
+            listRange -= 1;
+        }
+
+        public void drawJumper()
+        {
+            if (listRange > 2)
+            {
+                for (int i = listRange - 1; i > -1; i--)
+                {
+                    Console.WriteLine(jumper[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("   x   ");
+                for (int i = listRange - 1; i > -1; i--)
+                {
+                    Console.WriteLine(jumper[i]);
+                }
+            }
+                       
+        }
+
         
     }
+        
 }
